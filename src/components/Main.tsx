67b1f2ab5508ab42362f35d0ac24976fc6b6ee69
Nha Nguyen <nhant.guide@gmail.com>
2021-12-08 15:43:05 +0700

    feat: update color mode

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,18 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import { AdvancedChart } from "react-tradingview-embed";
 
-const Main = () => (
-    <Box w="1000px" h="500px">
-        <AdvancedChart
-            widgetProps={{
-                theme: "light",
-                symbol: "BINANCE:BTCUSDT",
-                hide_side_toolbar: true,
-                toolbar_bg: "white",
-            }}
-        />
-    </Box>
-);
+const Main = () => {
+    const { colorMode } = useColorMode();
 
+    return (
+        <Box w="1000px" h="500px">
+            <AdvancedChart
+                widgetProps={{
+                    theme: colorMode,
+                    symbol: "BINANCE:BTCUSDT",
+                    hide_side_toolbar: true,
+                    toolbar_bg: "transparent",
+                }}
+            />
+        </Box>
+    );
+};
 export default Main;
